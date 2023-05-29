7ff42dc46d5bc105e0c2c0c127af9d41aac1e648
shabit24 <hossain.shabit24@gmail.com>
2023-05-29 19:41:10 +0700

    no Pewarisam file

---
+++ noPewarisan/noPewarisan.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class baseClass final {
+public:
+	virtual void perkenalan() {
+		cout << "Hello saya function dari base class";
+	}
+};
+
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hello saya function dari derived class";
+	}
+};
+
+int main() {
+	derivedClass a;
+	a.perkenalan();
+	return 0;
+}
